e9768f2aab8c420c945a2438011ecb7075781120
Sridev Balakrishnan <sridev.balakrishnan@gmail.com>
2020-01-16 08:55:22 +0530

    Initial commit

---
+++ demo/src/main/java/com/helloworld/demo/controller/EmployeeController.java
@@ -0,0 +1,22 @@
+package com.helloworld.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.helloworld.demo.model.Employee;
+
+@RestController
+public class EmployeeController {
+	
+	@RequestMapping("/")
+    public List<Employee> getEmployees() 
+    {
+		List<Employee> employeesList = new ArrayList<Employee>();
+		employeesList.add(new Employee(1,"sridev","balakrishnan","sb@gmail.com"));
+		return employeesList;
+    }
+
+}
